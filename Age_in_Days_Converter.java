5422bcc7e3c0540f27f9946eda1d5a1e5655ede2
Kooreis <kooreis.official@gmail.com>
2022-02-11 13:00:00 -0600

    Imported the Scanner class from the java.util package to read user input.

--- Age_in_Days_Converter.java
+++ Age_in_Days_Converter.java
@@ -1,13 +1,1 @@
-```java
 import java.util.Scanner;
-
-public class AgeInDaysConverter {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your age in years: ");
-        int ageInYears = scanner.nextInt();
-        int ageInDays = ageInYears * 365;
-        System.out.println("Your age in days is: " + ageInDays);
-    }
-}
-```
