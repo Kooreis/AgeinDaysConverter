75eaf3fda56f22e500c2659ec7714ca56937fbb2
Kooreis <kooreis.official@gmail.com>
2022-02-11 13:00:00 -0600

    Created a new C# program with a Main method.

--- Age_in_Days_Converter.cs
+++ Age_in_Days_Converter.cs
@@ -1,15 +1,8 @@
-```CSharp
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter your age in years: ");
-        int years = Convert.ToInt32(Console.ReadLine());
-        int days = years * 365;
-        Console.WriteLine("Your age in days is: " + days);
-        Console.ReadKey();
     }
 }
-```
