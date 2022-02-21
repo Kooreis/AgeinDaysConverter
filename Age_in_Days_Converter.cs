3699cc4f36e4bb606f64d7a4a402b5e4197d4802
Kooreis <kooreis.official@gmail.com>
2022-02-21 13:00:00 -0600

    Initial setup of the program, prompting the user to enter their age in years.

--- Age_in_Days_Converter.cs
+++ Age_in_Days_Converter.cs
@@ -5,9 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Enter your age in years: ");
-        int years = Convert.ToInt32(Console.ReadLine());
-        int days = years * 365;
-        Console.WriteLine("Your age in days is: " + days);
-        Console.ReadKey();
     }
 }
